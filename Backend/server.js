2064bd00efe52e3ebd9ae32b52696311999c1612
btatanh28 <upskh2801@gmail.com>
2025-04-17 23:08:32 +0700

    Cập nhật Frontend và Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/AuthRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -17,7 +16,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
