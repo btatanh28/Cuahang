d8568f28fef26aea078e964da47a6543290ba044
btatanh28 <upskh2801@gmail.com>
2025-04-17 21:57:30 +0700

    Cập nhật Frontend và Backend

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/auth', require('./routes/AuthRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
