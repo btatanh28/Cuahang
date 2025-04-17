dce739056164cfd885c5d66279367bbe652f9c05
btatanh28 <upskh2801@gmail.com>
2025-04-18 00:25:59 +0700

    Cập nhật Frontend và Backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,18 +1,26 @@
+require('dotenv').config();  
+
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'tuananh28',
-    database: 'QuanLyBanHang'
+// Cấu hình pool kết nối
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,     
+    connectionLimit: 10,            
+    queueLimit: 0                
 });
 
-db.connect((err) => {
+// Sử dụng pool kết nối
+db.getConnection((err, connection) => {
     if (err) {
-        console.error('Error connecting to MySQL:', err);
+        console.error('Lỗi khi kết nối MySQL:', err);
         return;
     }
-    console.log('Connected to MySQL database');
+    console.log('Đã kết nối tới cơ sở dữ liệu MySQL');
+    connection.release();  
 });
 
 module.exports = db;
